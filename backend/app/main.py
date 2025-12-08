7bc5b6734914a9859e1530c931e4afec3c15ff08
Elma Vuran <elma.vuran@fer.unizg.hr>
2025-12-09 00:11:20 +0100

    creating a classroom. TODO fix classroom model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .routers import health, test_db
 from .routers.auth import router as auth
+from .routers.classroom_router import router as classroom_router
 
 
 
@@ -10,6 +11,7 @@ app = FastAPI(title="SmartMath API", version="0.1.0")
 app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(test_db.router, prefix="/test", tags=["test"])
 app.include_router(auth)
+app.include_router(classroom_router)
 
 @app.get("/")
 def root():
---
+++ backend/app/routers/classroom_router.py
@@ -0,0 +1,49 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from ..routers.auth import get_current_user
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from ..db import get_db
+from ..models.users import User
+from ..models.classroom import Classroom
+from ..models.user_classroom import user_classroom
+
+router = APIRouter(prefix="/classroom", tags=['classroom'])
+db_dependency = Annotated[Session, Depends(get_db)]
+
+class CreateClassroomRequest(BaseModel):
+    class_code: str
+    #TODO: dodati classroom name
+
+
+@router.post("/create", summary="Create new classroom")
+def create_new_classroom(request: CreateClassroomRequest,
+                        db: db_dependency,
+                        current_user: User = Depends(get_current_user)):
+    if current_user.role != "teacher":
+        raise HTTPException(status_code=403, detail="User does not have permission to create classrooms.")
+    
+    existing = db.query(Classroom).filter(Classroom.class_code == request.class_code).first() #vec postoji classroom sa tim kodom
+    if existing:
+        raise HTTPException(status_code=400, detail="Classroom with that class_code already exists.")
+
+    new_classroom = Classroom(
+        class_code=request.class_code
+        #TODO: dodati classroom_name kad ga stavimo u bazu
+        #TODO: dodati vezu na current usera tako da classroom ima direktnu vezu na nastavnika
+    )
+
+    db.add(new_classroom)
+    db.commit()
+    db.refresh(new_classroom)
+
+    query = user_classroom.insert().values(
+        user_id=current_user.id,
+        class_id=new_classroom.id
+    )
+
+    db.execute(query)
+    db.commit()
+
+    return {"message": "Classroom created", "classroom_id": new_classroom.id}
+
