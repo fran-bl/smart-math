b9b58d1fc1e06eba0804c05e4b8b90a9f0322978
Elma Vuran <elma.vuran@fer.unizg.hr>
2025-12-17 11:00:58 +0100

    Fixed class code generation. Started on sockets

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
+from backend.app.routers import game_router
 from fastapi import FastAPI
 from .routers import health, test_db
 from .routers.auth import router as auth
 from .routers.classroom_router import router as classroom_router
+from app import  socketio
 
 
 
@@ -12,8 +14,12 @@ app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(test_db.router, prefix="/test", tags=["test"])
 app.include_router(auth)
 app.include_router(classroom_router)
+app.include_router(game_router)
 
 @app.get("/")
 def root():
     return "Backend is running!"
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000)
 
